6dcdf644d1829b13183d84e4fb960109a420f511
Simon Berner <sibebzh@gmail.com>
2023-01-30 16:27:55 +0100

    add: :sparkles: setup the GameModel as the delegate

--- LADISimulator/Model/GameModel.swift
+++ LADISimulator/Model/GameModel.swift
@@ -7,7 +7,24 @@
 
 import Foundation
 
-// Is an observable object which can be consumed
-final class GameModel: ObservableObject {
-    
+/// The GameModel is the delegate by conforming to the delegate protocol. The GameModel can be observed.
+final class GameModel: ObservableObject, GameSimulatorDelegate {
+    @Published var gameState = GameState(homeTeamScore: 0, guestTeamScore: 0, scoringTeamName: "", lastAction: "")
+
+    let simulator = GameSimulator()
+
+    init() {
+        // Register the GameModel as delegate of the GameSimulator
+        simulator.delegate = self
+    }
+
+    // Update the state of the game
+    func didUpdate(gameState: GameState) {
+        self.gameState = gameState
+    }
+
+    func didCompleteGame() {
+        // TODO: implement
+    }
+
 }
